36d7eedb4bc7317eb51c610e9cf636c4aa0cd616
Markus Deutsch <moopat@users.noreply.github.com>
2015-03-06 19:13:51 +0100

    Cleanup. Data Streaming Only.

--- src/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef W_FALLALERT_H_
-#define W_FALLALERT_H_
-	
-void cancel_alert();
-	
-#endif
--- src/window_medication.h
+++
@@ -1,9 +0,0 @@
-#ifndef W_MEDICATION_H_
-#define W_MEDICATION_H_
-
-void medication_window_load(Window *window);
-void deinit_medication_window();
-void medication_window_unload(Window *window);
-void init_medication_window();
-	
-#endif
